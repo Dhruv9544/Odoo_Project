cd683c5ffe9176a13a31420046e36da22be555db
Uday Gohel <goheluday6445@gmail.com>
2024-07-14 08:56:57 +0530

    Done

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -2,23 +2,53 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { PrimeReactProvider } from "primereact/api";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-const App = () => {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <Login />,
-        },
-        {
-            path: "/register",
-            element: <Register />,
-        },
-    ]);
+import { Header as AdminHeader } from "./components/Admin/Header";
+import { Header } from "./components/User/Header";
+import ErrorPage from "./pages/ErrorPage";
+import { Dashboard } from "./pages/User/Dashboard";
+import { Dashboard as AdminDashboard } from "./pages/Admin/Dashboard";
 
-    return (
-        <PrimeReactProvider>
-            <RouterProvider router={router}></RouterProvider>
-        </PrimeReactProvider>
-    );
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/user",
+      element: <Header />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "",
+          element: <Dashboard />,
+          errorElement: <ErrorPage />,
+        },
+      ],
+    },
+    {
+      path: "/admin",
+      element: <AdminHeader />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "",
+          element: <AdminDashboard />,
+          errorElement: <ErrorPage />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <PrimeReactProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </PrimeReactProvider>
+  );
 };
 
 export default App;
---
+++ client-web/src/pages/Admin/Dashboard.jsx
@@ -0,0 +1,71 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { fetchGet, fetchPost } from "../../apis/fetch";
+
+export const Dashboard = () => {
+  const [data, setdata] = useState({});
+  const [visible, setvisible] = useState(false);
+  const navigate = useNavigate();
+
+  const fetchCount = async () => {
+    try {
+      const res = await fetchGet(
+        "furniture/getCount",
+        localStorage.getItem("token")
+      );
+
+      console.log(res);
+      setdata();
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCount();
+  }, []);
+
+  const AnimatedCount = ({ finalCount }) => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+      const animationDuration = 500; // in milliseconds
+      const steps = finalCount;
+      const stepDuration = animationDuration / steps;
+
+      let currentStep = 0;
+
+      const interval = setInterval(() => {
+        if (currentStep <= steps) {
+          setCount(currentStep);
+          currentStep += 1;
+        } else {
+          clearInterval(interval);
+        }
+      }, stepDuration);
+
+      return () => clearInterval(interval);
+    }, [finalCount]);
+
+    return <div className="font-bold p-3 text-4xl">{count}</div>;
+  };
+  return (
+    <div className="m-5">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 grid-rows-3">
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className="font-semibold p-5 text-2xl">Total Earning</div>
+          <AnimatedCount finalCount={50} />
+        </div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className="font-semibold p-5 text-2xl">Sells Count</div>
+          <AnimatedCount finalCount={50} />
+        </div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className="font-semibold p-5 text-2xl">Total Category</div>
+          <AnimatedCount finalCount={50} />
+        </div>
+      </div>
+    </div>
+  );
+};

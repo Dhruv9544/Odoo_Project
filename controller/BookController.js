2d6d12b17e7c226395ccb90d4b113fa505441db9
Uday Gohel <goheluday6445@gmail.com>
2024-07-14 13:43:18 +0530

    Book controller Update ,Advance search added in UI

--- controller/BookController.js
+++ controller/BookController.js
@@ -14,8 +14,25 @@ module.exports.addBook = catchAsync(async (req, res, next) => {
 });
 
 module.exports.getBooks = catchAsync(async (req, res, next) => {
-  const bookings = await bookModel.find(req.query);
-  res.json(bookings);
+  try {
+    const { title, author, genre } = req.query;
+    const searchCriteria = {};
+
+    if (title) {
+      searchCriteria.title = { $regex: title, $options: "i" };
+    }
+    if (author) {
+      searchCriteria.author = { $regex: author, $options: "i" };
+    }
+    if (genre) {
+      searchCriteria.genre = { $regex: genre, $options: "i" };
+    }
+
+    const books = await bookModel.find(searchCriteria);
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports.searchBook = catchAsync(async (req, res, next) => {

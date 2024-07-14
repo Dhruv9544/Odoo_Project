7f1bbe8960776ee86b5fcd575dbe7febd71055f0
Uday Gohel <goheluday6445@gmail.com>
2024-07-14 08:31:13 +0530

    Done

--- client-web/src/index.js
+++ client-web/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
+import store from "./store";
+import { Provider } from "react-redux";
+import "primereact/resources/themes/viva-light/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );

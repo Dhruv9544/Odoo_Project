0ecc415db0512528beb662925f1ff453a068a88d
MihirPatel3004 <mihirpatel@avianexperiences.com>
2024-07-14 15:40:24 +0530

    Errors solved

--- client-web/src/pages/User/AllBooks.jsx
+++ client-web/src/pages/User/AllBooks.jsx
@@ -4,87 +4,95 @@ import { Dropdown } from "primereact/dropdown";
 import { fetchGet } from "../../apis/fetch";
 
 const AllBooks = () => {
-  const [book, setBook] = useState([]);
-  const [selectedFilter, setSelectedFilter] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [search, setSearch] = useState(null);
+    const [book, setBook] = useState([]);
+    const [selectedFilter, setSelectedFilter] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState(null);
 
-  useEffect(() => {
-    getAllBook();
-  }, [search]);
+    useEffect(() => {
+        getAllBook();
+    }, [search]);
 
-  async function getAllBook() {
-    setLoading(true);
-    const response = await fetchGet(
-      `getBook?${selectedFilter}=${search}`,
-      localStorage.getItem("token")
-    );
-    setBook(response); // assuming response.data contains the book array
-    setLoading(false);
-  }
+    async function getAllBook() {
+        setLoading(true);
+        const response = await fetchGet(
+            `getBook?${selectedFilter}=${search}`,
+            localStorage.getItem("token")
+        );
+        setBook(response); // assuming response.data contains the book array
+        setLoading(false);
+    }
 
-  const filterOptions = [
-    { label: "Title", value: "title" },
-    { label: "Author", value: "author" },
-    { label: "Genre", value: "genre" },
-  ];
+    const filterOptions = [
+        { label: "Title", value: "title" },
+        { label: "Author", value: "author" },
+        { label: "Genre", value: "genre" },
+    ];
 
-  return (
-    <main className="bg-gray-100">
-      <div className="container mx-auto p-4">
-        <div className="flex flex-col lg:flex-row gap-4 mb-4">
-          <div className="flex-1">
-            <input
-              type="text"
-              placeholder="Search book"
-              className="border p-2 rounded w-full"
-              onChange={(e) => setSearch(e.target.value)}
-            />
-          </div>
-          <div className="flex-1">
-            <Dropdown
-              value={selectedFilter}
-              options={filterOptions}
-              onChange={(e) => setSelectedFilter(e.value)}
-              placeholder="Select a Filter"
-              className="w-full"
-            />
-          </div>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            book.map((item, index) => (
-              <div
-                className="bg-white min-w-[32%] max-w-[32%] rounded-lg shadow-lg overflow-hidden"
-                key={index}
-              >
-                <img
-                  src={`${item.image}`}
-                  className="w-full h-48 object-contain"
-                  alt={item.title ? item.title : "book"}
-                />
-                <div className="p-4">
-                  <Link to={`${item._id}`}>
-                    {item.title && (
-                      <h2 className="text-lg font-light">{item.title}</h2>
-                    )}
-                    {item.publisher && (
-                      <p className="text-gray-600">{item.publisher}</p>
-                    )}
-                    {item.year && (
-                      <p className="font-bold">Rent Price: {item.year / 100}</p>
-                    )}
-                  </Link>
+    return (
+        <main className="bg-gray-100">
+            <div className="container mx-auto p-4">
+                <div className="flex flex-col lg:flex-row gap-4 mb-4">
+                    <div className="flex-1">
+                        <input
+                            type="text"
+                            placeholder="Search book"
+                            className="border p-2 rounded w-full"
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex-1">
+                        <Dropdown
+                            value={selectedFilter}
+                            options={filterOptions}
+                            onChange={(e) => setSelectedFilter(e.value)}
+                            placeholder="Select a Filter"
+                            className="w-full"
+                        />
+                    </div>
                 </div>
-              </div>
-            ))
-          )}
-        </div>
-      </div>
-    </main>
-  );
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {loading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        book.map((item, index) => (
+                            <div
+                                className="bg-white min-w-[32%] max-w-[32%] rounded-lg shadow-lg overflow-hidden"
+                                key={index}
+                            >
+                                {console.log(item)}
+                                <img
+                                    src={`${item.image}`}
+                                    className="w-full h-48 object-contain"
+                                    alt={item.title ? item.title : "book"}
+                                />
+                                <div className="p-4">
+                                    <Link to={`${item._id}`}>
+                                        {console.log(item._id)}
+                                        {item.title && (
+                                            <h2 className="text-lg font-light">
+                                                {item.title}
+                                            </h2>
+                                        )}
+                                        {item.publisher && (
+                                            <p className="text-gray-600">
+                                                {item.publisher}
+                                            </p>
+                                        )}
+                                        {item.year && (
+                                            <p className="font-bold">
+                                                Rent Price: ₹ {item.price}
+                                            </p>
+                                        )}
+                                    </Link>
+                                </div>
+                            </div>
+                        ))
+                    )}
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default AllBooks;
--- controller/BookController.js
+++ controller/BookController.js
@@ -2,57 +2,60 @@ const bookModel = require("../model/bookmodel");
 const catchAsync = require("../utils/catchAsync");
 
 module.exports.addBook = catchAsync(async (req, res, next) => {
-  const newBook = new bookModel(req.body);
-  const book = await newBook.save();
+    const newBook = new bookModel(req.body);
+    const book = await newBook.save();
 
-  const url = `http://localhost:9999/uploads/${req.file.filename}`;
+    const url = `http://localhost:9999/uploads/${req.file.filename}`;
 
-  book.image = url;
-  await book.save();
+    book.image = url;
+    await book.save();
 
-  res.status(200).json({ book });
+    res.status(200).json({ book });
 });
 
 module.exports.getBooks = catchAsync(async (req, res, next) => {
-  try {
-    const { title, author, genre } = req.query;
-    const searchCriteria = {};
+    try {
+        const { title, author, genre, _id } = req.query;
+        const searchCriteria = {};
 
-    if (title) {
-      searchCriteria.title = { $regex: title, $options: "i" };
-    }
-    if (author) {
-      searchCriteria.author = { $regex: author, $options: "i" };
-    }
-    if (genre) {
-      searchCriteria.genre = { $regex: genre, $options: "i" };
-    }
+        if (title) {
+            searchCriteria.title = { $regex: title, $options: "i" };
+        }
+        if (author) {
+            searchCriteria.author = { $regex: author, $options: "i" };
+        }
+        if (genre) {
+            searchCriteria.genre = { $regex: genre, $options: "i" };
+        }
+        if (_id) {
+            searchCriteria._id = _id;
+        }
 
-    const books = await bookModel.find(searchCriteria);
-    res.status(200).json(books);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+        const books = await bookModel.find(searchCriteria);
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 module.exports.searchBook = catchAsync(async (req, res, next) => {
-  try {
-    const { title, author, genre } = req.query;
-    const searchCriteria = {};
+    try {
+        const { title, author, genre } = req.query;
+        const searchCriteria = {};
 
-    if (title) {
-      searchCriteria.title = { $regex: title, $options: "i" };
-    }
-    if (author) {
-      searchCriteria.author = { $regex: author, $options: "i" };
-    }
-    if (genre) {
-      searchCriteria.genre = { $regex: genre, $options: "i" };
-    }
+        if (title) {
+            searchCriteria.title = { $regex: title, $options: "i" };
+        }
+        if (author) {
+            searchCriteria.author = { $regex: author, $options: "i" };
+        }
+        if (genre) {
+            searchCriteria.genre = { $regex: genre, $options: "i" };
+        }
 
-    const books = await bookModel.find(searchCriteria);
-    res.status(200).json(books);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+        const books = await bookModel.find(searchCriteria);
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
